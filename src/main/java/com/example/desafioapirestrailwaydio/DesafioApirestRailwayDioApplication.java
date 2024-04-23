aed448b44058da9aa1b619a0ce9c6aa2abe192a1
Anderson Costa <andersonfelipedc@gmail.com>
2024-04-22 23:03:12 -0300

    Initial commit

---
+++ src/main/java/com/example/desafioapirestrailwaydio/DesafioApirestRailwayDioApplication.java
@@ -0,0 +1,13 @@
+package com.example.desafioapirestrailwaydio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioApirestRailwayDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioApirestRailwayDioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/desafioapirestrailwaydio/DesafioApirestRailwayDioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafioapirestrailwaydio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApirestRailwayDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
